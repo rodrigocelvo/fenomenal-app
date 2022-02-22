988c981a0997bccea78c70a4191d67cadf7c8754
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-02-21 22:40:21 -0300

    chore: add routers

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import  {  LogBox  }  from  'react-native' ;
+
+import Routes from './src/routes'
 
 export default function App() {
+LogBox . ignoreLogs ( [ 
+  "[react-native-gesture-handler] Parece que você está usando uma API antiga com componentes de gestos, confira o novo sistema de gestos!" , 
+] ) ;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
