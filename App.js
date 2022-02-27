092fb571561b16745e7a11b050ecab93a4f64972
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-02-27 13:47:34 -0300

    chore: added eslint

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import  {  LogBox  }  from  'react-native' ;
+import { LogBox } from 'react-native';
 
 import Routes from './src/routes'
 
 export default function App() {
-LogBox . ignoreLogs ( [ 
-  "[react-native-gesture-handler] Parece que você está usando uma API antiga com componentes de gestos, confira o novo sistema de gestos!" , 
-] ) ;
+  LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Parece que você está usando uma API antiga com componentes de gestos, confira o novo sistema de gestos!",
+  ])
 
   return (
     <Routes />
